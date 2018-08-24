c9f51fe694132aeedf66565c66b3b66a004043b0
pshenmic <pshenmic@gmail.com>
2018-08-24 18:47:31 +0700

    main entry refactor

--- app.js
+++ app.js
@@ -11,23 +11,14 @@ const AddressController = require('./app/controllers/AddressController');
 const BlockController = require('./app/controllers/BlockController');
 const TransactionController = require('./app/controllers/TransactionController');
 
-console.log(`Starting insight-api-bcoin v${version}`);
+const PORT = 3000;
 
-initComponents()
-    .then(() => {
-        console.log('Application initialized');
-        app.listen(3000)
-    })
-    .catch((e) => {
-        console.log('er', e);
-        process.exit(1)
-    });
+let _app = null;
 
+async function startApp() {
+    console.log(`Starting insight-api-bcoin v${version}`);
 
-async function initComponents() {
-    console.log('Initializing components');
-
-    async function initBcoinNode() {
+    async function setupBcoinNode() {
         config.memory = false;
         config['index-tx'] = true;
         config['index-address'] = true;
@@ -54,7 +45,7 @@ async function initComponents() {
     }
 
 
-    async function initKoa(node) {
+    async function setupKoa(node) {
 
         const addressController = new AddressController(node);
         const blockController = new BlockController(node);
@@ -108,11 +99,40 @@ async function initComponents() {
 
             .use(router.routes())
             .use(router.allowedMethods());
+
+
+        _app = app.listen(PORT);
+
+        _app.stopBcoin = async () => {
+            node.stopSync();
+            await node.close();
+        };
     }
 
-    let node = await initBcoinNode();
-    await initKoa(node)
+    console.log('Initializing components');
+    const node = await setupBcoinNode();
+    await setupKoa(node)
 }
 
+async function stopApp() {
+    if (!_app) {
+        throw new Error("App is not initialized")
+    }
+    await _app.close();
+    await _app.stopBcoin();
+}
 
+if (process.env.NODE_ENV !== 'test') {
+    startApp()
+        .then(() => {
+            console.log('Application initialized');
+        })
+        .catch((e) => {
+            console.log('er', e);
+            process.exit(1)
+        });
+}
+
+app.startApp = startApp;
+app.stopApp = stopApp;
 module.exports = app;
