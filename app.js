5d17811fb198bbbad1461ae1f34a089bd5116aa6
pshenmic <pshenmic@gmail.com>
2018-08-27 15:18:02 +0700

    remove response time middleware

--- app.js
+++ app.js
@@ -54,23 +54,6 @@ async function startApp() {
         //Body parser
         app.use(bodyParser());
 
-        // logger
-        app.use(async (ctx, next) => {
-            ctx.body = ctx.request.body;
-            await next();
-            const rt = ctx.response.get('X-Response-Time');
-            console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-        });
-
-        // x-response-time
-
-        app.use(async (ctx, next) => {
-            const start = Date.now();
-            await next();
-            const ms = Date.now() - start;
-            ctx.set('X-Response-Time', `${ms}ms`);
-        });
-
         // response
         const router = new Router();
 
