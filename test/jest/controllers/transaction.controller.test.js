6c0644004eb044fa06f96e63c337ac3efb4b28c7
pshenmic <pshenmic@gmail.com>
2018-08-28 15:50:10 +0700

    transaction controller tests - getTx test cases

--- test/jest/controllers/transaction.controller.test.js
+++ test/jest/controllers/transaction.controller.test.js
@@ -2,6 +2,10 @@ const app = require('../../../app');
 const request = require('supertest');
 const tx_020d3d = require('./expects/tx_020d3d');
 const tx_f3ea8a = require('./expects/tx_f3ea8a');
+const tx_74a79a = require('./expects/tx_74a79a');
+const tx_d822cf4 = require('./expects/tx_d822cf4');
+const tx_517c1b = require('./expects/tx_517c1b');
+const tx_d2359a = require('./expects/tx_d2359a');
 global.app = app;
 
 
@@ -20,14 +24,18 @@ function fixVinFields(responseBody, expected) {
     //also, doubleSpentTxID is always null in insight api (unimplemented option)
     expected.vin = expected.vin.map(vinElem => {
         //todo fix asm
-        delete vinElem.scriptSig.asm;
+        if (vinElem.scriptSig) {
+            delete vinElem.scriptSig.asm;
+        }
         delete vinElem.doubleSpentTxID;
         return vinElem;
     });
 
     responseBody.vin = responseBody.vin.map(vinElem => {
         //todo fix asm
-        delete vinElem.scriptSig.asm;
+        if (vinElem.scriptSig) {
+            delete vinElem.scriptSig.asm;
+        }
         return vinElem;
     });
 
@@ -77,6 +85,69 @@ describe('Get transaction test [/tx/:txid]', () => {
         fixVinFields(response.body, expected);
 
         expect(response.body).toEqual(expected);
+    });
+
+    test('Get transaction 74a79af4438404a55783e23077bfb0ad40c1c8c2ea584c2779ef4cdb31831819', async () => {
+        let response = await request(app.callback()).get('/tx/74a79af4438404a55783e23077bfb0ad40c1c8c2ea584c2779ef4cdb31831819');
+
+        let expected = tx_74a79a;
+
+        expect(response.status).toEqual(200);
+
+        expect(typeof response.body.confirmations === 'number').toBeTruthy();
+        expect(response.body.confirmations).toBeGreaterThanOrEqual(0);
+
+        //we have tested its the number, removing field
+        removeField('confirmations', response.body, expected);
+
+        //removing some unstable fields
+        fixVinFields(response.body, expected);
+
+        expect(response.body).toEqual(expected);
+    });
+
+
+    test('Get transaction d822cf4687fac2dad39d8c8bc0bacf929334b69e673ff5496a3e9b1fcb219039', async () => {
+        let response = await request(app.callback()).get('/tx/d822cf4687fac2dad39d8c8bc0bacf929334b69e673ff5496a3e9b1fcb219039');
+
+        let expected = tx_d822cf4;
+
+        expect(response.status).toEqual(200);
+
+        expect(typeof response.body.confirmations === 'number').toBeTruthy();
+        expect(response.body.confirmations).toBeGreaterThanOrEqual(0);
+
+        //we have tested its the number, removing field
+        removeField('confirmations', response.body, expected);
+
+        //removing some unstable fields
+        fixVinFields(response.body, expected);
+
+        expect(response.body).toEqual(expected);
+    });
+
+    test('Get transaction invalid transaction id (chars)', async () => {
+        let response = await request(app.callback()).get('/tx/zzzzz');
+
+
+        expect(response.status).toEqual(400);
+        expect(response.body.message).toEqual('Txid is not valid');
+
+    });
+
+    test('Get transaction invalid transaction id (random sha256)', async () => {
+        let response = await request(app.callback()).get('/tx/96c5fe264e9015ce925039aa84c6afefaf56180bc31950f3b1b7583e70137eee');
+
+
+        expect(response.status).toEqual(404);
+        expect(response.body.message).toEqual('Tx not found');
+
+    });
+
+    test('Get transaction without param', async () => {
+        let response = await request(app.callback()).get('/tx/');
+
+        expect(response.status).toEqual(404);
     });
 
 });
